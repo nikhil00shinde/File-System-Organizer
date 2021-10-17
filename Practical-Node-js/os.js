58b1ac1e0891d8c73e189eb79340566295eb3b46
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-17 10:54:36 +0530

    os module

---
+++ Practical-Node-js/os.js
@@ -0,0 +1,10 @@
+//node js-> use hota hain server bnane ke liye
+//os -> os se baat krne ke liye, os features
+
+let os = require("os");
+
+// console.log(os.arch()); x64
+// console.log(os.platform()); win32
+
+// console.log(os.networkInterfaces()); //wifi
+console.log(os.cpus());//server ka load pta kare
